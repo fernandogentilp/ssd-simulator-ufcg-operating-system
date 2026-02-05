0e8be738b45e14e37bee6a323f6b907d55229306
Fernando <gentilcx1@gmail.com>
2026-02-05 10:40:56 -0300

    adicionando o tempo de erase, pode ser modificado depois

--- simple_ssd/simple_ssd.py
+++ simple_ssd/simple_ssd.py
@@ -1,10 +1,17 @@
-def erase():
+from time import sleep
+
+
+def erase(flashblock):
     print("Erasing SSD...")
+    sleep(1) # Simula o tempo que toma para apagar o bloco flash  
+    
     
     
 def write():
     print("Writing to SSD...")
     
     
-def read():
+def read(page):
     print("Reading from SSD...")
+    
+    
