931127a74786bf52516b833476ea59b5f6bc1aa8
fernandogentilp <gentilcx1@gmail.com>
2026-08-30 20:11:38 -0300

    fazendo alterações nesse projeto depois de muito tempo. adicionando write e read.

--- simple_ssd/simple_ssd.py
+++ simple_ssd/simple_ssd.py
@@ -1,17 +1,41 @@
-from time import sleep
+import argparse
+class DirectMappedFTL:
+    def __init__(self, blocks, pages_per_block, tr, tp, te):
+        self.B = blocks
+        self.P = pages_per_block
+        self.TR = tr
+        self.TP = tp
+        self.TE = te
 
+        # no mapeamento direto, capacidade logica == capacidade fisica
+        self.capacidade_logica = blocks * pages_per_block
+        self.capacidade_fisica = blocks * pages_per_block
 
-def erase(flashblock):
-    print("Erasing SSD...")
-    sleep(1) # Simula o tempo que toma para apagar o bloco flash  
+        self.total_paginas_programadas = 0
+        self.total_escritas_logicas = 0
+        
     
+    def _endereco_fisico(self, lpn):
+        if lpn < 0 or lpn >= self.capacidade_logica:
+            raise ValueError(f"LPN {lpn} fora da capacidade lógica. ({self.capacidade_logica})")
+        bloco =  lpn // self.P
+        offset = lpn % self.P
+        return bloco, offset
     
+    def read(self, lpn):
+        self._endereco_fisico(lpn)
+        return self.TR
     
-def write():
-    print("Writing to SSD...")
-    
-    
-def read(page):
-    print("Reading from SSD...")
+    def write(self, lpn):
+        self._endereco_fisico(lpn)
+        self.total_escritas_logicas += 1
+        
+        custo_leitura = self.TR * self.P
+        custo_apagamento = self.TE 
+        custo_escrita = self.TP * self.P
+        
+        self.total_paginas_programadas += self.P
+        
+        return custo_apagamento + custo_escrita + custo_leitura
     
     
